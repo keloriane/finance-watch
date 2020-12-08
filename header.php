e93d895db2958c868095e73b28fc133b360b40af
keloriane <kevin.flbt@gmail.com>
2020-12-08 15:10:35 +0100

    responsive header

--- header.php
+++ header.php
@@ -5,5 +5,44 @@
   </head>
   <body>
     <header>
-    <div></div>
+        <div class="header-wrapper">
+
+    <div class="inner-header d-grid l-col-21 d-col-21 t-col-21 m-grid ">
+        <nav class="center">
+        <ul class="nav-bar">
+            <li> <a href=""> à propos</a> </li>
+            <li> <a href=""> Accès Membre</a> </li>
+            <li> <a href=""> Nos autres Blogs</a> </li>
+            <li> <a href=""> Newsletter</a> </li>
+            <li><a href=""> | </a>  </li>
+            <li><a href=""> <img src="<?php echo get_template_directory_uri() ?>/images/facebook-w.svg" alt=""> </a> </li>
+            <li><a href=""> <img src="<?php echo get_template_directory_uri() ?>/images/youtube.svg" alt=""> </a> </li>
+
+            </ul>
+
+        </nav>
+        |
+
+    </div>
+        <div class="sub-header">
+            <div class="logo-main">
+            <div class="logo-container">
+                <img src="" alt="">
+            </div>
+            <div class="header-title">
+                <h2>
+                    Changer la finance
+                </h2>
+                <h3>
+                    Un blog des membres et des amis de Finance Watch en France
+                </h3>
+            </div>
+            </div>
+            <div class="header-action">
+                <div class="search"></div>
+                <div class="participate"></div>
+                <div class="Financerr"></div>
+            </div>
+        </div>
+        </div>
   </header>
